c86a41ddd55cba83e74c427925ff2062750c07a7
José Lucas da Silva <joselucas0303@gmail.com>
2022-03-04 20:37:18 -0300

    feat: add base app

---
+++ src/android/app/src/main/kotlin/br/coodesh/challenge/flutter_challenge_202106/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.coodesh.challenge.flutter_challenge_202106
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ src/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'app/pages/home/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Coodesh Challenge',
+      home: const HomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.openSansTextTheme(),
+      ),
+    );
+  }
+}
